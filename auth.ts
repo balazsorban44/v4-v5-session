d4125a21b63db0b8b0a86303be9ed14b554bddc0
Balázs Orbán <info@balazsorban.com>
2025-02-13 10:47:37 +0100

    tweak

--- auth.ts
+++ auth.ts
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth";
 import { decode } from "next-auth/jwt";
 import GitHub from "next-auth/providers/github";
-import * as jose from "jose";
-import hkdf from "@panva/hkdf";
+import { jwtDecrypt } from "jose";
+import { hkdf } from "@panva/hkdf";
 
 const useSecureCookies = process.env.NODE_ENV === "production"; // Technically use secure cookies when you host from https://
 const cookiePrefix = useSecureCookies ? "__Secure-" : "";
@@ -11,15 +11,13 @@ export const { handlers, signIn, auth } = NextAuth({
   providers: [GitHub],
   jwt: {
     async decode(params) {
+      if (!params.token) return null;
       try {
         return await decode(params);
       } catch {
         const { token: jwt, secret, salt } = params;
-        if (!jwt) return null;
         const key = await getDerivedEncryptionKey([secret].flat()[0], salt);
-        const { payload } = await jose.jwtDecrypt(jwt, key, {
-          clockTolerance: 15,
-        });
+        const { payload } = await jwtDecrypt(jwt, key, { clockTolerance: 15 });
         return payload;
       }
     },
